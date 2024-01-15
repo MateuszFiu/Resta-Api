8fb2a9b7f48664a2f7bacc3aa39100b621dc97c5
MateuszFiu <148969658+MateuszFiu@users.noreply.github.com>
2024-01-16 00:09:55 +0100

    Update peopleController.php

--- app/Http/Controllers/peopleController.php
+++ app/Http/Controllers/peopleController.php
@@ -7,34 +7,45 @@ use App\Models\people;
 
 class peopleController extends Controller
 {
-    public function index()
-    {
-        return people::all();
-    }
-
-    public function store(Request $request)
-    {
-        $people = people::create($request->all());
-
-        return response()->json($people, 201);
-    }
-
-    public function show(people $people)
-    {
-        return response()->json($people);
-    }
-
-    public function update(Request $request, people $people)
-    {
-        $people->update($request->all());
-
-        return response()->json($user);
-    }
-
-    public function destroy(people $people)
-    {
-        $people->delete();
-
-        return response()->json(null, 204);
-    }
+        public function index()
+        {
+            return people::all();
+        }
+    
+        public function store(request $request)
+        {
+            // CREATE a new record
+            $validatedData = $request->validate([
+                'name' => 'required|string',
+                'surname' => 'required|string',
+                'phonenumber' => 'required|string',
+                'street' => 'required|string',
+                'city' => 'required|string',
+            ]);
+    
+            $person = People::create($validatedData);
+    
+            return response()->json($person, 201);
+        }
+    
+        public function show($id)
+        {
+            $people = People::find($id);
+            return response()->json($people);
+        }
+    
+        public function update(request $request, $id)
+        {
+            $people = People::find($id);
+            $people->update($request->all());
+            return response()->json($people, 200);
+        }
+    
+        public function destroy($id)
+        {
+            $people = People::find($id);
+            $people->delete();
+            return response()->json(null, 204);
+        }
 }
+
